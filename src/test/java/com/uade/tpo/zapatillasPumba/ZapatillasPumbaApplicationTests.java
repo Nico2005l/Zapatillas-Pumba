924464bf90216485b3e5a17f83d4ea51fda005a3
Nicolas Lussoro <lussoro8@gmail.com>
2025-08-26 17:05:10 -0300

    Crear Proyecto Zapatillas Pumba

---
+++ src/test/java/com/uade/tpo/zapatillasPumba/ZapatillasPumbaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.zapatillasPumba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapatillasPumbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
