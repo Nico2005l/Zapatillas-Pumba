6785197ccffc7932e812578df54c4216b3c0e0c9
Nicolas Lussoro <lussoro8@gmail.com>
2025-08-31 17:13:22 -0300

    copio codigo reutilizable

---
+++ src/main/java/com/uade/tpo/zapatillasPumba/service/CategoryService.java
@@ -0,0 +1,18 @@
+
+package com.uade.tpo.zapatillasPumba.service;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.uade.tpo.zapatillasPumba.entity.Category;
+import com.uade.tpo.zapatillasPumba.exceptions.CategoryDuplicateException;
+
+public interface CategoryService {
+    public Page<Category> getCategories(PageRequest pageRequest);
+
+    public Optional<Category> getCategoryById(Long categoryId);
+
+    public Category createCategory(String description) throws CategoryDuplicateException;
+}
