9f11097e246e3b026ad3f1d500730f73965ca78a
Erik Berns <erik.christian.berns@gmail.com>
2025-08-31 18:26:50 -0300

    Corrección repositories

--- src/main/java/com/uade/tpo/zapatillasPumba/repository/ProductRepository.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/repository/ProductRepository.java
@@ -1,13 +1,14 @@
 package com.uade.tpo.zapatillasPumba.repository;
 
+
 import com.uade.tpo.zapatillasPumba.entity.Product;
-
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("SELECT p FROM Product p WHERE p.category.id = ?1")
     List<Product> findByCategoryId(Long categoryId);
 }
