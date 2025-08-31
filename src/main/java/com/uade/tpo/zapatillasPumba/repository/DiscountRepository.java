a238956eb4e42ce649d80ebc7c58d453ca61b532
Erik Berns <erik.christian.berns@gmail.com>
2025-08-31 17:53:09 -0300

    ProductRepository

--- src/main/java/com/uade/tpo/zapatillasPumba/repository/DiscountRepository.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/repository/DiscountRepository.java
@@ -1,9 +1,3 @@
 package com.uade.tpo.zapatillasPumba.repository;
 
-public class DiscountRepository extends JpaRepository<Category, Long>{
 
-    public void applyDiscount(Order order, Discount discount) {
-        // Logic to apply discount to the order
-    }
-
-}
--- src/main/java/com/uade/tpo/zapatillasPumba/repository/ProductRepository.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/repository/ProductRepository.java
@@ -1,17 +1,13 @@
 package com.uade.tpo.zapatillasPumba.repository;
 
+import com.uade.tpo.zapatillasPumba.entity.Product;
+
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import com.uade.tpo.zapatillasPumba.entity.Product;
-
 @Repository
-public class ProductRepository extends JpaRepository<Product, Long> {
-
-    @Query(value = "SELECT p FROM Product p WHERE p.category.id =  ?1")
+public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategoryId(Long categoryId);
-
 }
