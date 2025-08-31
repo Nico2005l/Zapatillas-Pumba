11268396218d9e8063e84641d50b7bcba2504138
Nicolas Lussoro <lussoro8@gmail.com>
2025-08-31 18:57:22 -0300

    fix

--- src/main/java/com/uade/tpo/zapatillasPumba/repository/UserRepository.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/repository/UserRepository.java
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-package com.uade.tpo.zapatillasPumba.repository;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.uade.tpo.zapatillasPumba.entity.User;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String mail);
-}
-=======
 package com.uade.tpo.zapatillasPumba.repository;
 
 
@@ -28,4 +13,3 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> findByEmail(String email);
 }
->>>>>>> 0960b0bd4e2fe743aebea15caaf3cbad8e261667
