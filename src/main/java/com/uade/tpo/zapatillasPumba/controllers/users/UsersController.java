566bb3d2aa0acca7ec07548830e279625d71eae2
Erik Berns <erik.christian.berns@gmail.com>
2025-09-02 15:01:49 -0300

    UsersController

--- src/main/java/com/uade/tpo/zapatillasPumba/controllers/users/UsersController.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/controllers/users/UsersController.java
@@ -1,5 +1,46 @@
 package com.uade.tpo.zapatillasPumba.controllers.users;
 
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import java.util.List;
+
+import com.uade.tpo.zapatillasPumba.entity.User;
+import com.uade.tpo.zapatillasPumba.service.UserService;
+
+@RestController
+@RequestMapping("users")
 public class UsersController {
-    
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	public ResponseEntity<List<User>> getUsers() {
+		List<User> users = userService.getAllUsers();
+		return ResponseEntity.ok(users);
+	}
+
+	@GetMapping("/{userId}")
+	public ResponseEntity<User> getUserById(@PathVariable Long userId) {
+		User user = userService.getUserById(userId);
+		if (user != null) {
+			return ResponseEntity.ok(user);
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping("/{userId}")
+	public ResponseEntity<Void> updateUser(
+		@PathVariable Long userId,
+		@RequestBody User user) {
+		userService.updateUser(userId, user);
+		return ResponseEntity.noContent().build();
+	}
+
+	@DeleteMapping("/{userId}")
+	public ResponseEntity<Void> deleteUser(@PathVariable Long userId) {
+		userService.deleteUser(userId);
+		return ResponseEntity.noContent().build();
+	}
 }
