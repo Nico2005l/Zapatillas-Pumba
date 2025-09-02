a5f84bbe8494869ee01bea58eeb7a5b14bffb177
Erik Berns <erik.christian.berns@gmail.com>
2025-09-02 14:23:45 -0300

    "@Repository"

--- src/main/java/com/uade/tpo/zapatillasPumba/repository/OrderRepository.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/repository/OrderRepository.java
@@ -4,7 +4,9 @@ import com.uade.tpo.zapatillasPumba.entity.Order;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 	@Query("select o from Order o where o.user.id = ?1")
 	List<Order> findByUserId(Long userId);
