d6afeadcca8aa91b50ac8c5866ea279ae362d8ac
Erik Berns <erik.christian.berns@gmail.com>
2025-09-02 15:06:19 -0300

    UserRequest

--- src/main/java/com/uade/tpo/zapatillasPumba/controllers/users/UserRequest.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/controllers/users/UserRequest.java
@@ -1,5 +1,13 @@
 package com.uade.tpo.zapatillasPumba.controllers.users;
 
+import lombok.Data;
+
+@Data
 public class UserRequest {
-    
+	private String username;
+	private String email;
+	private String password;
+	private String firstName;
+	private String lastName;
+	private String role;
 }
