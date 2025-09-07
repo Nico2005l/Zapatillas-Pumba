864376445018b090e0804209c0e68f0e0ed21101
Nicolas Lussoro <lussoro8@gmail.com>
2025-09-07 17:42:37 -0300

    impl de order item

--- src/main/java/com/uade/tpo/zapatillasPumba/service/OrderItem/OrderItemServiceImpl.java
+++ src/main/java/com/uade/tpo/zapatillasPumba/service/OrderItem/OrderItemServiceImpl.java
@@ -14,28 +14,7 @@ public class OrderItemServiceImpl implements OrderItemService {
     private OrderItemRepository orderItemRepository;
 
     @Override
-    public List<OrderItem> getAllOrderItems() {
-        return orderItemRepository.findAll();
-    }
-
-    @Override
-    public OrderItem getOrderItemById(Long id) {
-        return orderItemRepository.findById(id).orElse(null);
-    }
-
-    @Override
-    public OrderItem createOrderItem(OrderItem orderItem) {
-        return orderItemRepository.save(orderItem);
-    }
-
-    @Override
-    public OrderItem updateOrderItem(Long id, OrderItem orderItem) {
-        orderItem.setId(id);
-        return orderItemRepository.save(orderItem);
-    }
-
-    @Override
-    public void deleteOrderItem(Long id) {
-        orderItemRepository.deleteById(id);
+    public List<OrderItem> getOrderItemsByOrderId(Long orderId) {
+        return orderItemRepository.findByOrderId(orderId);
     }
 }
